53b3f1d87c6ae58216b7a92295120d9235fb92c6
LukeisStuff <lukeisawesome03@gmail.com>
2026-01-04 16:29:40 -0500

    update break result list

--- src/main/java/luke/randomite/BlockLogicOreRandomite.java
+++ src/main/java/luke/randomite/BlockLogicOreRandomite.java
@@ -24,7 +24,7 @@ public class BlockLogicOreRandomite extends BlockLogic {
     @Override
     public ItemStack[] getBreakResult(World world, EnumDropCause dropCause, int x, int y, int z, int meta, TileEntity tileEntity) {
         if (IS_DEEP_LOADED) {
-            int roll = world.rand.nextInt(100);
+            int roll = world.rand.nextInt(200);
 
             switch (dropCause) {
                 case SILK_TOUCH:
@@ -32,26 +32,26 @@ public class BlockLogicOreRandomite extends BlockLogic {
                     return new ItemStack[]{new ItemStack(this)};
                 case EXPLOSION:
                 case PROPER_TOOL:
-                    if (roll <= 30) return new ItemStack[]{new ItemStack(Items.EGG_CHICKEN)};
-                    if (roll <= 50) return new ItemStack[]{new ItemStack(Items.COAL)};
+                    if (roll < 60) return new ItemStack[]{new ItemStack(Items.EGG_CHICKEN)};
 
-                    if (roll <= 55) return new ItemStack[]{new ItemStack(Items.ORE_RAW_IRON)};
-                    if (roll <= 60) return new ItemStack[]{new ItemStack(Items.ORE_RAW_GOLD)};
+                    if (roll < 100) return new ItemStack[]{new ItemStack(Items.COAL)};
 
-                    if (roll <= 65) return new ItemStack[]{new ItemStack(DEEPItems.rawLead)};
-                    if (roll <= 70) return new ItemStack[]{new ItemStack(DEEPItems.rawSilver)};
+                    if (roll < 120) return new ItemStack[]{new ItemStack(Items.ORE_RAW_IRON)};
+                    if (roll < 140) return new ItemStack[]{new ItemStack(DEEPItems.rawLead)};
 
-                    if (roll <= 75) return new ItemStack[]{new ItemStack(Items.DYE, 4 + world.rand.nextInt(5), 4)};
-                    if (roll <= 80) return new ItemStack[]{new ItemStack(Items.DUST_REDSTONE, 1 + world.rand.nextInt(2))};
+                    if (roll < 150) return new ItemStack[]{new ItemStack(Items.ORE_RAW_GOLD)};
+                    if (roll < 160) return new ItemStack[]{new ItemStack(DEEPItems.rawSilver)};
 
-                    if (roll <= 85) return new ItemStack[]{new ItemStack(DEEPItems.rhodonite)};
-                    if (roll <= 90) return new ItemStack[]{new ItemStack(DEEPItems.uranium)};
+                    if (roll < 168) return new ItemStack[]{new ItemStack(Items.DYE, 4 + world.rand.nextInt(5), 4)};
+                    if (roll < 176) return new ItemStack[]{new ItemStack(Items.DUST_REDSTONE, 1 + world.rand.nextInt(4))};
+                    if (roll < 184) return new ItemStack[]{new ItemStack(DEEPItems.uranium)};
 
-                    if (roll <= 93) return new ItemStack[]{new ItemStack(Items.DIAMOND)};
+                    if (roll < 190) return new ItemStack[]{new ItemStack(Items.DIAMOND)};
+                    if (roll < 196) return new ItemStack[]{new ItemStack(DEEPItems.amethyst)};
 
-                    if (roll <= 97) return new ItemStack[]{new ItemStack(DEEPItems.amethyst)};
+                    if (roll < 198) return new ItemStack[]{new ItemStack(Items.INGOT_STEEL_CRUDE)};
+                    return new ItemStack[]{new ItemStack(DEEPItems.bismuth)};
 
-                    return new ItemStack[]{new ItemStack(Items.INGOT_STEEL_CRUDE)};
                 default:
                     return null;
             }
@@ -66,11 +66,15 @@ public class BlockLogicOreRandomite extends BlockLogic {
             case EXPLOSION:
             case PROPER_TOOL:
                 if (roll <= 30) return new ItemStack[]{new ItemStack(Items.EGG_CHICKEN)};
+
                 if (roll <= 50) return new ItemStack[]{new ItemStack(Items.COAL)};
+
                 if (roll <= 60) return new ItemStack[]{new ItemStack(Items.ORE_RAW_IRON)};
                 if (roll <= 70) return new ItemStack[]{new ItemStack(Items.ORE_RAW_GOLD)};
+
                 if (roll <= 80) return new ItemStack[]{new ItemStack(Items.DYE, 4 + world.rand.nextInt(5), 4)};
                 if (roll <= 90) return new ItemStack[]{new ItemStack(Items.DUST_REDSTONE, 1 + world.rand.nextInt(2))};
+
                 if (roll <= 95) return new ItemStack[]{new ItemStack(Items.DIAMOND)};
                 return new ItemStack[]{new ItemStack(Items.INGOT_STEEL_CRUDE)};
             default:
